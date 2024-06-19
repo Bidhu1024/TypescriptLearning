3b7cd7a1580454930f4f326833765e329c1d1b28
Bidhu1024 <bidhu1024av@gmail.com>
2024-06-19 10:36:10 +0530

    learnt about diff types in Typescript

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-var log = "df";
-console.log(log);
+// const inputs = document.querySelectorAll("input")
+// console.log(inputs);
+// const log = "df"
+// console.log(log);
+// const square = (num:number)=>{
+//     return num * num;
+// }
+// console.log(square(9));
+// let names = ["luigi", "mario", "yoshi"];
+// names.push("toad");
+// names.push(6);
+//explicit types in js
+var character;
+var age;
+var isBlackBelt;
+//arrays
+var ninja = [];
+//union type
+var mixed = [];
+//objects
+var ninjaOne;
+ninjaOne = {
+    name: "Yoi",
+    age: 29
+};
--- index.ts
+++ index.ts
@@ -1,4 +1,38 @@
-const inputs = document.querySelectorAll("input")
-console.log(inputs);
-const log = "df"
-console.log(log);
+// const inputs = document.querySelectorAll("input")
+// console.log(inputs);
+// const log = "df"
+// console.log(log);
+
+// const square = (num:number)=>{
+//     return num * num;
+// }
+// console.log(square(9));
+
+// let names = ["luigi", "mario", "yoshi"];
+// names.push("toad");
+// names.push(6);
+
+
+//explicit types in js
+let character:string;
+let age:number;
+let isBlackBelt:boolean;
+
+//arrays
+
+let ninja: string[]=[];
+
+//union type
+
+let mixed:(string | number )[] = []
+
+
+//objects
+
+let ninjaOne: object;
+ninjaOne={
+    name:"Yoi",
+    age:29
+}
+
+
